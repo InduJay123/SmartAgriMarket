3f31a970e00b59cbf37cabf56b388712d1d1127d
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2025-12-01 15:32:17 +0530

    route update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,19 @@ import Home from './scenes/home'
 import Navbar from './scenes/navbar'
 import Admin from './pages/admin/Admin';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import AIModel from './pages/admin/AIModel';
+import ManageBuyers from './pages/admin/ManageBuyers';
+import ManageCrops from './pages/admin/ManageCrops';
+import ManageFarmers from './pages/admin/ManageFarmers';
+import Reports from './pages/admin/Reports';
+import UploadPrice from './pages/admin/UploadPrice';
+import AddCrops from './pages/farmer/AddCrops';
+import AiInsights from './pages/farmer/AiInsights';
+import FarmerDashboard from './pages/farmer/FarmerDashboard';
+import Messages from './pages/farmer/Messages';
+import Settings from './pages/farmer/Settings';
+import SideBarLayout from './layout/SidebarLayout';
 
 function App() {
 
@@ -27,6 +40,26 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route path="/farmer" element={<SideBarLayout />}>
+          <Route path='dashboard' element={<FarmerDashboard/>} />
+          <Route path="ai-insights" element={<AiInsights />} />
+          <Route path="addcrops" element={<AddCrops />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+
+         <Route path="/admin" element={<Admin />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="farmers" element={<ManageFarmers />} />
+          <Route path="buyers" element={<ManageBuyers />} />
+          <Route path="crops" element={<ManageCrops />} />
+          <Route path="upload" element={<UploadPrice />} />
+          <Route path="reports" element={<Reports />} />
+          <Route path="ai" element={<AIModel />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-
-import ReactDOM from "react-dom/client";
-
 import "./i18n";
 
 createRoot(document.getElementById('root')!).render(
    <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
 )

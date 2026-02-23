13041e70842c9e19376d2236cfcfe5e725592ff3
Nuwan Ganganath <nuwanrathnayaka53@gmail.com>
2026-02-23 22:21:19 +0530

    feat: add localization for favourites page and update UI to use translated strings

--- src/pages/buyer/FavouritesPage.tsx
+++ src/pages/buyer/FavouritesPage.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import { fetchFavourites } from '../../api/favourites';
 import type { Product } from '../../@types/Product';
 import ProductGrid from '../../components/buyer/ProductGrid';
+import { useTranslation } from 'react-i18next';
 
 
 function FavouritesPage() {
   const [favouriteProducts, setFavouriteProducts] = useState<Product[]>([]);
+  const { t, i18n } = useTranslation();
+  const isSinhala = i18n.language === "si";
 
   useEffect(() => {
     const loadFavourites = async () => {
@@ -21,8 +24,8 @@ function FavouritesPage() {
 
   return (
     <div className="p-6">
-      <h1 className="text-4xl text-black font-bold px-4 py-2">🔖 Your Favourites</h1>
-      <p className="text-md text-gray-500 mb-6 px-4">Keep your frequently used items in one place</p>
+      <h1 className="text-4xl text-black font-bold px-4 py-2">🔖 {t("Your Favourites")}</h1>
+      <p className="text-md text-gray-500 mb-6 px-4">{t("Keep your frequently used items in one place")}</p>
       <ProductGrid products={favouriteProducts}  />
     </div>
   );

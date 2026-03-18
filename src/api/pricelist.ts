33014cdcdbab01ebeed769443fe3d0534284f95f
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-03-18 23:24:39 +0530

    feat: Update API endpoints in UploadPrice and GetPriceListPDF for consistency and add custom filename input

--- src/api/pricelist.ts
+++ src/api/pricelist.ts
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const GetPriceListPDF = async () => {
     try{
-        const response = await api.get(`/prices/uploads/`);
+        const response = await api.get(`/documents/price-lists/`);
         console.log(response.data);
         return Array.isArray(response.data) ? response.data : [];
     }catch(err){

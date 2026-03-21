0f9359da036a75529024461375a45d5616ff8af7
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-03-22 00:33:26 +0530

    Initial commit

--- src/@types/emoji-dictionary.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "emoji-dictionary" {
-  const emoji: {
-    getUnicode(name: string): string;
-    getName(unicode: string): string | null;
-  };
-  export default emoji;
-}
--- src/api/farmer/alerts.ts
+++
@@ -1,9 +0,0 @@
-import api from "../api";
-
-export const fetchUserAlerts = (limit: number = 100) => {
-  return api.get(`/alerts/alerts/?limit=${limit}`);
-};
-
-export const markAlertSeen = () => {
-  return api.post(`/alerts/mark-seen/`);
-};
